0ee5bc04fe4ee3f849ec7ee606ee4b6ec44b92c6
Muhammad Zohaib <128612800+MuhammadZohaib105663@users.noreply.github.com>
2024-04-03 07:59:20 +0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,21 +2,21 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['923421048558']
+global.ownername = "Muhammad Zohaib"//owner name
+global.ytname = "YT: Zohaib Botz.inc"
+global.socialm = "GitHub: MuhammadZohaib105663"
+global.location = "Pakistani"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'My-GPT' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'My GPT'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "Zohaib botz inc."
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
